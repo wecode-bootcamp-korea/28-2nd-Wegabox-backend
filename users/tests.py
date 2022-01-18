7a9472079b909e87d81799124dec91c0f6de0edd
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2022-01-18 18:55:01 +0900

    [28기 강민성]Add: 소셜로그인 기능 구현

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+import json
+import unittest
 
-# Create your tests here.
+from unittest      import mock
+from unittest.mock import MagicMock, patch
+from django.test   import TestCase, Client
+
+from users.models import User
+
+class KakaoSignInTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id = 1,
+            kakao_id = 11111111,
+            nickname = '모모',
+            email = 'momo@gmail.com'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+    
+    @patch('users.views.requests')
+    def test_kakao_signin_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id'            : 11111111,
+                    'properties'    : {'nickname': '모모'}, 
+                    'kakao_account' : {'email': 'momo@gmail.com'}
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'HTTP_Authorization': 'fake_access_token'}
+        response            = client.get("/users/login", **headers)
+        self.assertEqual(response.status_code, 200)
+    
+    @patch('users.views.requests')
+    def test_kakao_signin_key_error(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id'            : 11111111,
+                    'properties'    : {'nickname': '모모'}
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        headers             = {'HTTP_Authorization': 'fake_access_token'}
+        response            = client.get("/users/login", **headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'KEY_ERROR'})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import json
+import jwt
+import requests
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+from my_settings  import SECRET_KEY, ALGORITHM
+
+class KakaoClient:
+    def __init__(self, access_token):
+        self.API_URL = 'https://kapi.kakao.com/v2/user/me'
+        self.headers = {'Authorization': f'Bearer {access_token}'}
+
+    def get_user_profile(self):
+        response = requests.get(self.API_URL, headers = self.headers, timeout=10)
+        return response.json()
+
+class KakaoSignInView(View):
+    def get(self, request):
+        try:
+            access_token = request.headers['Authorization'] 
+
+            kakao_client = KakaoClient(access_token)
+            profile = kakao_client.get_user_profile()
+
+            kakao_account = profile['kakao_account']
+            kakao_id = profile['id']
+            properties = profile['properties']
+            email = kakao_account['email']
+            nickname = properties['nickname']
+
+            user, created  = User.objects.get_or_create(
+                kakao_id = kakao_id,
+                email    = email,
+                nickname = nickname
+            )
+            jwt_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm=ALGORITHM)   
+
+            return JsonResponse({
+                'token'    : jwt_token,
+                'nickname' : user.nickname,
+                'kakao_id' : user.kakao_id,
+                'email'    : user.email
+            }, status = 200)
+            
+        
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
